fcccb497fb1ecdeb14956ca69ecd8aaee74e7388
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2024-12-16 07:52:41 +0530

    feat(layout.tsx): add Google Tag Manager script for enhanced analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,17 @@ export default function RootLayout({
           content="We build high-performance web applications and websites."
         ></meta>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NMQS73V7');
+    `,
+          }}
+        ></script>
+        <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-11298597203"
         ></script>
@@ -63,6 +74,11 @@ export default function RootLayout({
         ></script>
       </head>
       <body className={`${jetBrainsMono.className} antialiased w-full`}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMQS73V7"
+          height="0" width="0" style={{
+            display: "none",
+            visibility: "hidden",
+          }}></iframe></noscript>
         <Navbar />
         {children}
         <Analytics />
