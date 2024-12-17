ec9a6a46696b45c0d18d1f2c7ed732a765f08ba9
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2024-12-17 15:35:45 +0530

    fix export

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -1,4 +1,4 @@
-export function Unauthorized() {
+export default function Unauthorized() {
     return (
         <div className="min-h-screen flex items-center justify-center">
             <div className="text-center">
